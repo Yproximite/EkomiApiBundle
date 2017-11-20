37bd9d0573f17625876a4e44e2cdf8f4c69da8dd
Gatou Muriel <muriel@yproximite.com>
2017-11-20 18:16:27 +0100

    WIP Fix Spec

--- spec/DependencyInjection/YproximiteEkomiApiExtensionSpec.php
+++ spec/DependencyInjection/YproximiteEkomiApiExtensionSpec.php
@@ -3,6 +3,7 @@
 namespace spec\Yproximite\Bundle\EkomiApiBundle\DependencyInjection;
 
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -29,19 +30,20 @@ class YproximiteEkomiApiExtensionSpec extends ObjectBehavior
             ],
         ];
 
-        $container->setAlias('yproximite.ekomi_api.http_client', 'httplug.client.guzzle6')->shouldBeCalled();
+        $container->setAlias(Argument::exact('yproximite.ekomi_api.http_client'), Argument::exact('httplug.client.guzzle6'))->shouldBeCalled();
 
         $arguments = [new Reference('yproximite.ekomi_api.http_client'), 'xxxx', 'yyyy', 'http://api.host.com'];
 
         $client = new Definition(Client::class, $arguments);
         $client->setPublic(false);
 
-        $container->setDefinition('yproximite.ekomi_api.client', $client)->shouldBeCalled();
+        $container->setDefinition(Argument::exact('yproximite.ekomi_api.client'), Argument::exact($client))->shouldBeCalled();
 
         $clientRef  = new Reference('yproximite.ekomi_api.client');
         $aggregator = new Definition(ServiceAggregator::class, [$clientRef]);
+        $aggregator->setPublic(true);
 
-        $container->setDefinition('yproximite.ekomi_api.service_aggregator', $aggregator)->shouldBeCalled();
+        $container->setDefinition(Argument::exact('yproximite.ekomi_api.service_aggregator'), Argument::exact($aggregator))->shouldBeCalled();
 
         $this->load($configs, $container);
     }
