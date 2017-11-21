c8c3dabc84cf033881c539a4190949d705d43438
Gatou Muriel <muriel@yproximite.com>
2017-11-21 10:31:36 +0100

    Fix Spec test

--- spec/DependencyInjection/YproximiteEkomiApiExtensionSpec.php
+++ spec/DependencyInjection/YproximiteEkomiApiExtensionSpec.php
@@ -3,7 +3,6 @@
 namespace spec\Yproximite\Bundle\EkomiApiBundle\DependencyInjection;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -30,20 +29,28 @@ class YproximiteEkomiApiExtensionSpec extends ObjectBehavior
             ],
         ];
 
-        $container->setAlias(Argument::exact('yproximite.ekomi_api.http_client'), Argument::exact('httplug.client.guzzle6'))->shouldBeCalled();
+        $container->setAlias('yproximite.ekomi_api.http_client', 'httplug.client.guzzle6')->shouldBeCalled();
 
-        $arguments = [new Reference('yproximite.ekomi_api.http_client'), 'xxxx', 'yyyy', 'http://api.host.com'];
+        $arguments = [
+            new Reference('yproximite.ekomi_api.http_client'),
+            'xxxx',
+            'yyyy',
+            'http://api.host.com',
+            null,
+            null,
+            'yproximite.ekomi.cache_key',
+        ];
 
         $client = new Definition(Client::class, $arguments);
         $client->setPublic(false);
 
-        $container->setDefinition(Argument::exact('yproximite.ekomi_api.client'), Argument::exact($client))->shouldBeCalled();
+        $container->setDefinition('yproximite.ekomi_api.client', $client)->shouldBeCalled();
 
         $clientRef  = new Reference('yproximite.ekomi_api.client');
         $aggregator = new Definition(ServiceAggregator::class, [$clientRef]);
         $aggregator->setPublic(true);
 
-        $container->setDefinition(Argument::exact('yproximite.ekomi_api.service_aggregator'), Argument::exact($aggregator))->shouldBeCalled();
+        $container->setDefinition('yproximite.ekomi_api.service_aggregator', $aggregator)->shouldBeCalled();
 
         $this->load($configs, $container);
     }
